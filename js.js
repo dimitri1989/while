e246714d801a39f7e33db8ce697c5d3ecf9b6dc6
dimitri1989 <dimitri.kvarelashvili@gmail.com>
2023-05-02 14:57:38 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+let sum = [1, 2, -9, -56, 34, 77, -90, -11, -1, -2, -5, -9];
+let x = 0;
+while(x <= sum.length){
+    if(sum[x]<0){
+        document.write(sum[x] + "<br>");
+    }
+    x++;
+
+
+}
